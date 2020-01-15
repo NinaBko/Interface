a618969729eec385f882a247231467e74d8386c0
M. Dauriac Alexandre <aldauria@insa-20543>
2020-01-15 18:05:56 +0100

    BDD class connection OK

--- src/Database/BDD.java
+++ src/Database/BDD.java
@@ -5,22 +5,37 @@ import java.io.*;
 
 public class BDD {
 
-    public BDD(){
-        action();
-    }
+    private Connection con;
+    private Statement stmt;
 
-    public void action(){
+    public BDD(){
         try{
             Class.forName("com.mysql.jdbc.Driver");
-            Connection con=DriverManager.getConnection(
-                    "jdbc:mysql://srv-bdens.insa-toulouse.fr:3306/tpservlet_04","tpservlet_04","ua1laeW1");
+            this.con=DriverManager.getConnection("jdbc:mysql://srv-bdens.insa-toulouse.fr:3306/tpservlet_04","tpservlet_04","ua1laeW1");
+            this.stmt=con.createStatement();
 
-            Statement stmt=con.createStatement();
-            ResultSet rs=stmt.executeQuery("select * from user");
-            while(rs.next())
-                System.out.println(rs.getString(1));
-            con.close();
+
         }catch(Exception e){ System.out.println(e);}
+    }
+
+
+    public void printTableUser(){
+        try {
+            ResultSet rs = stmt.executeQuery("select * from user");
+            while (rs.next())
+                System.out.println(rs.getString(1));
+        }catch(Exception e){
+            System.out.println(e);
+        }
+    }
+
+    public void closeDatabase(){
+        try{
+            this.con.close();
+        }
+        catch(SQLException e){
+            System.out.println("Error close database");
+        }
     }
 
 
